7133b3cc3e200d47249a0b7e61920d188abbe846
Ricardo Zanivan <zanivanr@gmail.com>
2024-09-24 18:55:59 -0300

    add the people table if it does not already exist

--- node/index.js
+++ node/index.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const app = express();
 
@@ -8,19 +9,38 @@ const config = {
     password: 'root',
     database:'nodedb'
 };
-const mysql = require('mysql2');
 const connection = mysql.createConnection(config);
 
-const sql = `INSERT INTO people(name) values('Ricardo')`
-connection.query(sql)
-connection.end()
+const createTableSql = `
+    CREATE TABLE IF NOT EXISTS people (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    );
+`;
 
-// Rota padrão para testar se o servidor está funcionando
+connection.query(createTableSql, (err, result) => {
+    if (err) {
+        console.error('Erro ao criar a tabela:', err);
+        return;
+    }
+
+    const insertSql = `INSERT INTO people(name) VALUES('Ricardo')`;
+    connection.query(insertSql, (err, result) => {
+        if (err) {
+            console.error('Erro ao inserir dados:', err);
+            return;
+        }
+        console.log('Registro inserido com sucesso!');
+    });
+
+    connection.end();
+});
+
 app.get('/', (req, res) => {
     res.send('<h1>Full Cycle Rocks!</h1>');
 });
 
-// Inicia o servidor na porta 4200
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
