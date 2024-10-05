1d2d6fad38432f68fe16f8b641ce9b2b19e93ccc
Ricardo Zanivan <zanivanr@gmail.com>
2024-10-05 10:46:55 -0300

    Add people list in table to return in endpoint

--- node/index.js
+++ node/index.js
@@ -25,20 +25,64 @@ connection.query(createTableSql, (err, result) => {
         return;
     }
 
-    const insertSql = `INSERT INTO people(name) VALUES('Ricardo')`;
-    connection.query(insertSql, (err, result) => {
-        if (err) {
-            console.error('Erro ao inserir dados:', err);
-            return;
-        }
-        console.log('Registro inserido com sucesso!');
-    });
-
     connection.end();
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>');
+function doGenerateHtmlTable(title, peoples) {
+    let responseHtml = `
+        <h1>${title}</h1>
+        <table border="1" cellpadding="10" cellspacing="0" style="border-collapse: collapse; width: 50%; margin-top: 20px;">
+            <thead>
+                <tr>
+                    <th style="text-align: right;">#</th>
+                    <th style="text-align: left;">Nome</th>
+                </tr>
+            </thead>
+            <tbody>`;
+
+    if (peoples.length > 0) {
+        peoples.forEach((person, index) => {
+            responseHtml += `<tr>
+                                <td style="text-align: right;">${index + 1}</td>
+                                <td style="text-align: left;">${person.name}</td>
+                             </tr>`;
+        });
+    } else {
+        responseHtml += `<tr>
+                            <td colspan="2" style="text-align: center;">Nenhuma pessoa encontrada.</td>
+                         </tr>`;
+    }
+
+    responseHtml += `
+            </tbody>
+        </table>
+    `;
+
+    return responseHtml;
+}
+
+
+app.get('/', async (req, res) => {
+    let connection;
+    try {
+        connection = mysql.createConnection(config).promise();
+
+        const insertSql = `INSERT INTO people(name) VALUES('Ricardo')`;
+        await connection.query(insertSql);
+
+        const query = `SELECT * FROM people ORDER BY name`;
+        const [peoples] = await connection.query(query);
+
+        const htmlContent = doGenerateHtmlTable('Full Cycle Rocks!', peoples);
+
+        res.send(htmlContent);
+    } catch (err) {
+        res.status(500).send('Erro ao buscar pessoas');
+    } finally {
+        if (connection) {
+            await connection.end();
+        }
+    }
 });
 
 app.listen(port, () => {
